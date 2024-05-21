8b6a727f7ab18d951be752ecb70852726bd26b70
Paul Adams <ghusiyy@gmail.com>
2024-05-20 19:47:16 -0700

    move APOD code into separate module to clean up dashboard rendering

--- src/apod/index.ts
+++ src/apod/index.ts
@@ -1,7 +1,56 @@
-import { render } from "../utils";
+import { getAPOD, render } from "../utils";
 import "./style.css";
 
 export function setupAPOD(element: HTMLElement) {
   if (!element) return;
-  render(element, `<div>Hello, World!</div>`);
+
+  // Define the initial state interface
+  interface APODState {
+    apodData: Maybe<APOD>;
+  }
+
+  // Define the dashboard component
+
+  let state: APODState = {
+    apodData: null,
+  };
+
+  function setAPODData(data: any) {
+    state.apodData = data;
+    renderAPOD(state);
+  }
+
+  getAPOD().then((res) => setAPODData(res));
+
+  // Initial render
+  renderAPOD(state);
+
+  // Render function for rendering the dashboard UI
+  function renderAPOD(state: APODState) {
+    const dashboardHTML = `
+          ${
+            state.apodData
+              ? `<div class="APOD--container">
+                    <h3>${
+                      state.apodData.media_type === "video" ? "Video" : "Image"
+                    } of The Day</h3>
+                    <h3>${state.apodData.title} - <span>${
+                  state.apodData.date
+                }</h3>
+                    <div class="Media--container">
+                      ${
+                        state.apodData.media_type === "video"
+                          ? `<iframe width="560" height="315" src=${state.apodData.url} frameborder="0" allowfullscreen></iframe>
+                        `
+                          : `<img src=${state.apodData.url} alt="Picture of the Day"/>`
+                      }
+                    </div>
+                </div>`
+              : `<div class="shimmer loader"></div>`
+          }
+      `;
+
+    // Render dashboard UI
+    render(element, dashboardHTML);
+  }
 }
--- src/dashboard/index.ts
+++ src/dashboard/index.ts
@@ -4,22 +4,13 @@ import { getAPOD, render } from "../utils";
 // Define the initial state interface
 interface DashboardState {
   count: number;
-  apodData: Maybe<APOD>;
 }
 
 // Define the dashboard component
 export function setupDashboard(element: HTMLDivElement) {
   let state: DashboardState = {
     count: 0,
-    apodData: null,
   };
-
-  function setAPODData(data: any) {
-    state.apodData = data;
-    renderDashboard(state);
-  }
-
-  getAPOD().then((res) => setAPODData(res));
 
   // Initial render
   renderDashboard(state);
@@ -35,32 +26,10 @@ export function setupDashboard(element: HTMLDivElement) {
   }
 
   // Render function for rendering the dashboard UI
-  function renderDashboard(state: DashboardState) {
+  function renderDashboard(_state: DashboardState) {
     const dashboardHTML = `
       <div class="Dashboard--container">
-      <!--TODO: move to apod component -->
-        <div id="APOD">
-          ${
-            state.apodData
-              ? `<div class="APOD--container">
-                    <h3>${
-                      state.apodData.media_type === "video" ? "Video" : "Image"
-                    } of The Day</h3>
-                    <h3>${state.apodData.title} - <span>${
-                  state.apodData.date
-                }</h3>
-                    <div class="Media--container">
-                      ${
-                        state.apodData.media_type === "video"
-                          ? `<iframe width="560" height="315" src=${state.apodData.url} frameborder="0" allowfullscreen></iframe>
-                        `
-                          : `<img src=${state.apodData.url} alt="Picture of the Day"/>`
-                      }
-                    </div>
-                </div>`
-              : `<div class="shimmer loader"></div>`
-          }
-        </div>
+        <div id="APOD"></div>
       </div>
     `;
 
